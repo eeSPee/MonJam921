a966124eaab9afa629c245c7f921c7132b179e77
shertigan <51873824+shertigan@users.noreply.github.com>
2021-09-25 18:00:57 +0200

    mushroom talk

--- Assets/Script/Interactables/Mushroom.cs
+++ Assets/Script/Interactables/Mushroom.cs
@@ -4,6 +4,35 @@ using UnityEngine;
 
 public class Mushroom : MonoBehaviour
 {
+    public AudioSource AudioSourceMushroom;
+    public AudioClip[] AudioClipMushroom;
+
+    public float MinListenerDistance=1;
+    public float MaxListenerDistance=25;
+
+    private void Update()
+    {
+      UpdateListener();
+    }
+
+    public void UpdateListener()
+    {
+      float ListenerDistance = Vector3.Distance(transform.position, PlayerController.player.transform.position);
+
+      if (ListenerDistance <= MinListenerDistance)
+      {
+        AudioSourceMushroom.volume = 1;
+      }
+      else if (ListenerDistance > MaxListenerDistance)
+      {
+        AudioSourceMushroom.volume = 0;
+      }
+      else
+      {
+        AudioSourceMushroom.volume = 1 - ((ListenerDistance-MinListenerDistance) / (MaxListenerDistance - MinListenerDistance));
+      }
+    }
+
     Animator animator;
     private void Awake()
     {
@@ -15,7 +44,9 @@ public class Mushroom : MonoBehaviour
         {
             PlayerController player = collision.gameObject.GetComponent<PlayerController>();
             if (player.IsOriginal())
-            { 
+            {
+            //AudioSourceMushroom.clip = AudioClipMushroom[Random.Range(0, AudioClipMushroom.Length)];
+            AudioSourceMushroom.PlayOneShot(AudioClipMushroom[Random.Range(0, AudioClipMushroom.Length)]);
             animator.SetBool("Speaking", true);
             animator.SetFloat("AnimSpeed", 3);
             }
